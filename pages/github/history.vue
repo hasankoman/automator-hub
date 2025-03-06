5a6204f864f143802ae1fe206ba777af26196738
Hasan Koman <hasankoman94@gmail.com>
2025-03-06 14:30:22 +0300

    Refactor GitHub history page with PanelMenu for improved repository history display

--- pages/github/history.vue
+++ pages/github/history.vue
@@ -23,6 +23,37 @@ const formatDate = (dateString) => {
   const date = new Date(dateString);
   return date.toLocaleString();
 };
+
+const formattedHistory = computed(() => {
+  const repositoriesMap = new Map();
+
+  history.value.forEach((h) => {
+    if (!repositoriesMap.has(h.repositoryId)) {
+      repositoriesMap.set(h.repositoryId, {
+        label: h.repositoryName,
+        id: h.repositoryId,
+        icon: "hugeicons:ease-curve-control-points",
+        child: false,
+        items: [],
+      });
+    }
+
+    repositoriesMap.get(h.repositoryId).items.push({
+      label: `${h.operationType
+        ?.charAt(0)
+        .toUpperCase()}${h.operationType?.slice(1)} Update`,
+      status: h.status,
+      icon:
+        h.status === "success"
+          ? "hugeicons:checkmark-square-04"
+          : "hugeicons:multiplication-sign-square",
+      time: formatDate(h.createdAt),
+      child: true,
+    });
+  });
+
+  return Array.from(repositoriesMap.values());
+});
 </script>
 
 <template>
@@ -58,32 +89,39 @@ const formatDate = (dateString) => {
         </p>
       </div>
       <div class="grid grid-cols-1 auto-rows-min gap-5" v-else>
-        <div
-          v-for="h in history"
-          :key="h.id"
-          class="flex items-center justify-between gap-4 bg-white rounded-xl shadow-xs p-4 border border-gray-200"
+        <PanelMenu
+          :model="formattedHistory"
+          class="w-full"
+          :pt="{
+            panel: '!bg-white !rounded-xl !shadow-xs border border-gray-200',
+            headercontent: '!rounded-lg',
+            item: '!rounded-lg overflow-hidden',
+            itemcontent: '!bg-transparent',
+            rootlist: 'flex flex-col gap-2 !py-2',
+          }"
         >
-          <div class="flex flex-col gap-1 w-full">
-            <div class="flex justify-between items-center gap-2">
-              <h3 class="font-medium text-gray-900">
-                {{ h.repositoryName }}
-              </h3>
-              <Badge
-                :severity="h.status"
-                :value="h.status"
-                :pt="{
-                  root: '!rounded-lg px-3 py-1.5 !min-w-auto',
-                }"
-              />
-            </div>
-            <span class="text-sm text-gray-500">
-              {{ h.operationType === "manual" ? "Manual" : "Auto" }}
-            </span>
-            <span class="text-sm text-gray-500">
-              {{ formatDate(h.createdAt) }}
-            </span>
-          </div>
-        </div>
+          <template #item="{ item }">
+            <a
+              v-ripple
+              class="flex items-center p-2 group"
+              :class="
+                item.child
+                  ? item.status === 'success'
+                    ? '!bg-green-50 text-green-600 cursor-default '
+                    : '!bg-red-50 text-red-600 cursor-default '
+                  : ' cursor-pointer px-4 py-3 select-none'
+              "
+            >
+              <Icon :name="item.icon" />
+              <span :class="['ml-2', { 'font-semibold': item.items }]">{{
+                item.label
+              }}</span>
+              <span class="ml-auto text-sm">
+                {{ item.time }}
+              </span>
+            </a>
+          </template>
+        </PanelMenu>
       </div>
     </div>
   </div>
